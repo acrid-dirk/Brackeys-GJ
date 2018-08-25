d51c3b1e99ace987ccfce0dbe36d7350e7d5dcc0
Unknown <acrid.develop@gmail.com>
2018-08-25 18:37:27 -0500

    Pushed an example player controller
    Added an example player controller with scene. Needs feedback.

---
+++ GJUnityProject/Assets/Scripts/ExamplePlayerController.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// Enums
+public enum characterStates { Idle, Flying };
+
+public class ExamplePlayerController : MonoBehaviour {
+
+	[Header("References")]
+	[SerializeField] Transform charCamera;
+	Rigidbody rb;
+
+	[Header("Inputs")]
+	[SerializeField] float mouseSensitivity = 2f;
+	[SerializeField] float launchPower = 5f;
+	[SerializeField] KeyCode launchKey = KeyCode.Space; // Going to change to unity input later.
+	characterStates characterState = characterStates.Idle;
+	float pitch = 0;
+	float yaw = 0;
+	bool mouseLocked = false;
+	
+	void Awake(){
+		// Cache the rigidbody at awake.
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void Update () {
+		// Do mouse look first.
+		SimulateMouse();
+		LockMouse();
+
+		// Switch our states and call the function for each.
+		switch(characterState){
+			case(characterStates.Idle):
+				IdleInput();
+				break;
+			case(characterStates.Flying):
+				// Saved for later use.
+				break;
+		}
+	}
+
+	void LockMouse(){
+		// Check if the user pressed the left mouse button in game.
+		if(Input.GetKeyDown(KeyCode.Mouse0)){
+			// Lock the mouse if they did.
+			mouseLocked = true;
+		}
+		else if(Input.GetKeyDown(KeyCode.Escape)){
+			// If the user pressed escape than unlock the mouse.
+			mouseLocked = false;
+		}
+
+		if(mouseLocked){
+			Cursor.lockState = CursorLockMode.Locked;
+			Cursor.visible = false;
+		}else{
+			Cursor.lockState = CursorLockMode.None;
+			Cursor.visible = true;
+		}
+	}
+
+	void IdleInput(){
+		// When the character is 'idle' (Stuck on a wall) let them launch off of it
+		if(Input.GetKeyDown(launchKey)){
+			characterState = characterStates.Flying;
+			rb.AddForce(charCamera.forward * launchPower);
+		}
+	}
+
+	void SimulateMouse(){
+		// Mouse look, Limits the up/down to 85 degrees. Only updates the camera transform due to the player transform not needing any rotation updates.
+		yaw += (Input.GetAxisRaw("Mouse X") * mouseSensitivity);
+		yaw %= 360;
+
+		pitch += (-Input.GetAxisRaw("Mouse Y") * mouseSensitivity);
+		pitch = Mathf.Clamp(pitch, -85, 85);
+
+		charCamera.localRotation = Quaternion.Euler(pitch, yaw, 0);
+
+		Vector3 currentRotation = transform.localEulerAngles;
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		// Kill the player if they touch something deadly, aka light.
+		if(other.gameObject.CompareTag("Death")){
+			// TODO: Add a death screen
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+		}
+		// Only remove velocity and set us idle if we are flying
+		if(characterState == characterStates.Flying){
+			rb.velocity = Vector3.zero;
+			characterState = characterStates.Idle;
+		}
+	}
+}
