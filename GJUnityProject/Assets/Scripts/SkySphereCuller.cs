58c56547be29df39c90d75bedf9c84f78980537a
Chris <32988106+Noobot9k@users.noreply.github.com>
2018-08-28 03:48:55 -0600

    Added a simple level model.

---
+++ GJUnityProject/Assets/Scripts/SkySphereCuller.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkySphereCuller : MonoBehaviour {
+
+	public float CullDistance = 20f;
+	
+	// Update is called once per frame
+	void Update () {
+        float Distance = (Camera.main.transform.position - transform.position).magnitude;
+        Vector3 lookAtVector = (transform.position - Camera.main.transform.position).normalized;
+        float Dot = Vector3.Dot(lookAtVector, Camera.main.transform.forward);
+        print(Dot);
+        gameObject.GetComponent<MeshRenderer>().enabled = Distance < CullDistance || Dot > 0;
+	}
+}
