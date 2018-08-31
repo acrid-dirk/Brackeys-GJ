c2a2e0387745defe093f7a52b228e04d0b949fc0
ImKodi <imkodiofficiaL@GMAIL.COM>
2018-08-31 04:40:57 -0400

    Added Crystal Script
    Added a script in order to pick up the crystal. And complete the level.

---
+++ GJUnityProject/Assets/Scripts/LightCrystal.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LightCrystal : MonoBehaviour {
+
+	public float textUpTimer;
+
+	public bool hasCrystal = false;
+
+	public GameObject crystal;
+	public GameObject endPanel;
+	public Transform crystalTransform;
+
+	public Text crystalAquiredText;
+
+	void Start () {
+
+	}
+	void Update () {
+		textUpTimer += Time.fixedDeltaTime;
+		crystal.transform.Rotate(Vector3.up * Time.deltaTime * 10);
+
+		if(hasCrystal && textUpTimer <= 2){
+			crystalAquiredText.text = "Light crystal aquired";
+		}else{
+			crystalAquiredText.text = "";
+		}
+
+	}
+	public void OnCollisionEnter(Collision other){
+		if(other.collider.tag == "Crystal"){
+			hasCrystal = true;
+			textUpTimer = 0f;
+			crystal.SetActive(false);
+		}
+		if(other.collider.tag == "End" && hasCrystal){
+			crystalAquiredText.text = "";
+			endPanel.SetActive(true);
+		}
+	}
+}
